ff3b24d38a8bc852268b656e9665b73e6c87fab0
thienbao1505 <bao1552000@gmail.com>
2020-06-29 14:28:37 +0700

    a

---
+++ categories.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.0.2
+ */
+
+/**
+ * Database `baocc`
+ */
+
+/* `baocc`.`categories` */
+$categories = array(
+  array('cat_desc' => 'Loream','cat_id' => '1','cat_title' => 'Bearbrick Basic'),
+  array('cat_desc' => 'Lorem','cat_id' => '2','cat_title' => 'Bearbrick '),
+  array('cat_desc' => 'Lorem','cat_id' => '3','cat_title' => 'Bearbrick Luxury')
+);
